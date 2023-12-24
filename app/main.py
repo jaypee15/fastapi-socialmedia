3d2768d8dc767a87df09d32674f0fe119c9fd281
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-24 13:47:30 +0100

    first commit

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from database.db import init_db
+init_db()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
